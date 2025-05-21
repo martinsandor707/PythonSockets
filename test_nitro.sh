eb2fb438f2526259867b5f370fa8e2eb0db6d68a
martinsandor707 <martinsandor707@gmail.com>
2025-05-21 21:25:20 +0200

    Magic commit Pt. 2

--- test_nitro.sh
+++ test_nitro.sh
@@ -4,7 +4,7 @@ set -e
 
 # This script is used to create a hello world enclave application using the hello app located at /usr/share/nitro_enclaves/examples/hello
 
-docker build /usr/share/nitro_enclaves/examples/hello -t hello
-nitro-cli build-enclave --docker-uri hello:latest --output-file hello.eif
-nitro-cli run-enclave --cpu-count 2 --memory 512 --enclave-cid 16 --eif-path hello.eif --debug-mode
+docker build -t enclave-test .
+nitro-cli build-enclave --docker-uri enclave-test:latest --output-file enclave-test.eif
+nitro-cli run-enclave --cpu-count 2 --memory 1024 --enclave-cid 16 --eif-path enclave-test.eif --debug-mode
 nitro-cli describe-enclaves
