27f1e1b140a2c2fc8aea0ca557635451bd91a21f
martinsandor707 <martinsandor707@gmail.com>
2025-06-18 22:42:33 +0200

    modify test_nitro.sh

--- test_nitro.sh
+++ test_nitro.sh
@@ -6,6 +6,7 @@ set -e
 # It assumes that the Nitro Enclaves CLI and Docker are already installed and configured.
 # It also assumes that the user has the necessary permissions to run Docker and Nitro Enclaves commands.
 
+docker rmi -f $(docker images -a -q) # Remove all Docker images
 docker build -t enclave-test .
 nitro-cli build-enclave --docker-uri enclave-test:latest --output-file enclave-test.eif
 nitro-cli run-enclave --cpu-count 2 --memory 1024 --enclave-cid 16 --eif-path enclave-test.eif --debug-mode
